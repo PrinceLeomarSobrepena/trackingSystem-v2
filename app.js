4e8d25af452f441b11377b2a4fed7410953600b7
PrinceLeomarSobrepena <princeeeleomar0317@gmail.com>
2024-11-25 14:26:54 +0800

    change

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.get('*', (req, res) => {
 
 
 // Start the server
-// app.listen(3000, () => {
-//   console.log('Server is running on http://localhost:3000');
-// });
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
